9ee36f704701f1066ec70628b06afb298bd852a8
programmercito <noexisto@mail.com>
2025-10-15 19:41:31 -0400

    errores terminados

--- src/main/java/com/devsu/finapp/common/advices/GlobalExceptionHandler.java
+++ src/main/java/com/devsu/finapp/common/advices/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.devsu.finapp.common.advices;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +27,51 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(error);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<MessageError> manejarMiExcepcion(SQLIntegrityConstraintViolationException ex) {
+        MessageError error = new MessageError();
+        String message = ex.getMessage();
+        HttpStatus status = null;
+        if (message.contains("UK_")) {
+            String value = extractDuplicateValueForUKPrefix(message);
+            if (value != null) {
+                error.setMessage("The value '" + value + "' already exists in the system.");
+            } else {
+                error.setMessage("A unique constraint violation occurred.");
+            }
+            status = HttpStatus.CONFLICT;
+        } else {
+            error.setMessage("A database error occurred.");
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        error.setCode(getCode());
+        log.error(error, ex);
+        return ResponseEntity
+                .status(status)
+                .body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<MessageError> manejarMiExcepcion(Exception ex) {
+        MessageError error = new MessageError();
+        error.setMessage("An unexpected error occurred. Please try again later.");
+        error.setCode(getCode());
+        log.error(error, ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(error);
+    }
+
+    public static String extractDuplicateValueForUKPrefix(String errorMessage) {
+        String regex = "Duplicate entry '([^']+)' for key 'UK_[^']+'";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(errorMessage);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
     }
 
     public String getCode() {
