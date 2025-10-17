5bdec3c7c26366e4d4375ab6ce4f054a68f59dbc
programmercito <noexisto@mail.com>
2025-10-17 09:56:46 -0400

    files extras

---
+++ files/cuentas.sql
@@ -0,0 +1,115 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.7.2-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: cuentas
+-- ------------------------------------------------------
+-- Server version	12.0.2-MariaDB-ubu2404
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `clientes_lite`
+--
+
+DROP TABLE IF EXISTS `clientes_lite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes_lite` (
+  `id` bigint(20) NOT NULL,
+  `apellidos` varchar(255) NOT NULL,
+  `estado` bit(1) NOT NULL,
+  `lastupdate` datetime(6) NOT NULL,
+  `nombres` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes_lite`
+--
+
+LOCK TABLES `clientes_lite` WRITE;
+/*!40000 ALTER TABLE `clientes_lite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes_lite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cuentas`
+--
+
+DROP TABLE IF EXISTS `cuentas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuentas` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `estado` bit(1) NOT NULL,
+  `saldo` decimal(10,2) NOT NULL,
+  `tipo` varchar(255) NOT NULL,
+  `cliente_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_CLIENTE_CUENTA` (`cliente_id`),
+  CONSTRAINT `FK_CLIENTE_CUENTA` FOREIGN KEY (`cliente_id`) REFERENCES `clientes_lite` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuentas`
+--
+
+LOCK TABLES `cuentas` WRITE;
+/*!40000 ALTER TABLE `cuentas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cuentas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movimientos`
+--
+
+DROP TABLE IF EXISTS `movimientos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movimientos` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `estado` bit(1) NOT NULL,
+  `fecha` datetime(6) NOT NULL,
+  `movimiento` decimal(10,2) NOT NULL,
+  `saldo` decimal(10,2) NOT NULL,
+  `cuenta_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_CUENTA_MOVIMIENTO` (`cuenta_id`),
+  CONSTRAINT `FK_CUENTA_MOVIMIENTO` FOREIGN KEY (`cuenta_id`) REFERENCES `cuentas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movimientos`
+--
+
+LOCK TABLES `movimientos` WRITE;
+/*!40000 ALTER TABLE `movimientos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movimientos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'cuentas'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2025-10-17  3:24:07
